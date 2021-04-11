a69bbf1750b1cea480936242146fdd1feed29b46
zheng yi <zhengyi@Jonny-zheng.local>
2021-04-11 17:35:26 +0800

    finished

--- init.rb
+++ init.rb
@@ -1,8 +1,11 @@
+require_dependency 'select2_hook_listener'
+
 Redmine::Plugin.register :assignable_select2 do
   name 'Assignable Select2 plugin'
   author 'jonnyzheng'
   description 'This is a replacement for Redmine assignable select input'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/jonnyzheng/assignable_select2'
+  author_url 'https://github.com/jonnyzheng'
 end
+
---
+++ lib/select2_hook_listener.rb
@@ -0,0 +1,3 @@
+class Select2HookListener < Redmine::Hook::ViewListener
+    render_on :view_issues_form_details_bottom, partial: "issues/select2"
+end
