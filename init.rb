74292a658e122cc02c4567fc41f4eb296c6fa8f6
jonnyzheng <mars131@gmail.com>
2022-06-15 11:06:42 +0800

    update version number

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :assignable_select2 do
   name 'Assignable Select2 plugin'
   author 'jonnyzheng'
   description 'This is a replacement for Redmine assignable select input'
-  version '0.0.1'
+  version '0.2.0'
   url 'https://github.com/jonnyzheng/assignable_select2'
   author_url 'https://github.com/jonnyzheng'
 end
